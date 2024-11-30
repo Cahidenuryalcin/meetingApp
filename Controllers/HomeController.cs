9a278fa0c733dcaf09d5b16a0a7a432e8bd38c70
Cahidenuryalcin <cnuryal@gmail.com>
2024-11-30 23:11:38 +0300

    viewbag

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,14 @@ namespace MeetingApp.Controllers
 {
     public class HomeController : Controller{
         public IActionResult Index(){
+
+            int saat = DateTime.Now.Hour;
+            // ViewBag.Selamlama = saat > 12 ? "İyi günler":"Günaydın";
+            // ViewBag.UserName="Cahide";
+
+            ViewData["Selamlama"]=saat > 12 ? "İyi günler":"Günaydın";
+            ViewData["UserName"]="Cahide";
+            // model kalıbına girmeyen bir içeriği ViewData/ViewBag ile sayfaya gönderebilirsin.
             return View();
         }
     }
